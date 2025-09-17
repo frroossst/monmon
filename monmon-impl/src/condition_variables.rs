c073279be38d39bc606d1130be3f37777213413f
Adhyan H <adhyanpatel@gmail.com>
2025-09-17 16:43:36 -0600

    minor refactor

--- monmon-impl/src/condition_variables.rs
+++ monmon-impl/src/condition_variables.rs
@@ -1,4 +1,9 @@
+use std::sync::atomic::{AtomicU32, Ordering};
+use atomic_wait::{wait, wake_one, wake_all};
+
+
 use crate::semaphore::BinarySemaphore;
+
 
 #[derive(Debug)]
 pub struct Condition {
@@ -12,5 +17,71 @@ impl Default for Condition {
             waiting: 0,
             sem: BinarySemaphore::new(0),
         }
+    }
+}
+
+
+/// A condition variable implementation using futexes
+#[derive(Debug)]
+pub struct FutexCondition {
+    /// Number of threads waiting on this condition
+    waiting: AtomicU32,
+    /// Futex word for this condition variable
+    futex_word: AtomicU32,
+}
+
+impl FutexCondition {
+    pub fn new() -> Self {
+        FutexCondition {
+            waiting: AtomicU32::new(0),
+            futex_word: AtomicU32::new(0),
+        }
+    }
+
+    /// Wait on this condition variable
+    pub fn wait(&self) {
+        // Increment waiting count
+        self.waiting.fetch_add(1, Ordering::AcqRel);
+        
+        // Get current futex value
+        let current_val = self.futex_word.load(Ordering::Acquire);
+        
+        // Wait on the futex
+        wait(&self.futex_word, current_val);
+        
+        // We've been woken up, decrement waiting count
+        self.waiting.fetch_sub(1, Ordering::AcqRel);
+    }
+
+    /// Signal one waiting thread
+    pub fn signal(&self) -> bool {
+        let waiting_count = self.waiting.load(Ordering::Acquire);
+        if waiting_count > 0 {
+            // Change the futex word value to wake waiters
+            self.futex_word.fetch_add(1, Ordering::AcqRel);
+            // Wake one waiter
+            wake_one(&self.futex_word);
+            true
+        } else {
+            false
+        }
+    }
+
+    /// Broadcast to all waiting threads
+    pub fn broadcast(&self) -> u32 {
+        let waiting_count = self.waiting.load(Ordering::Acquire);
+        if waiting_count > 0 {
+            // Change the futex word value to wake waiters
+            self.futex_word.fetch_add(1, Ordering::AcqRel);
+            // Wake all waiters
+            wake_all(&self.futex_word);
+            waiting_count
+        } else {
+            0
+        }
+    }
+
+    pub fn waiting_count(&self) -> u32 {
+        self.waiting.load(Ordering::Acquire)
     }
 }
--- monmon-impl/src/monitors.rs
+++ monmon-impl/src/monitors.rs
@@ -1,9 +1,12 @@
 use std::os::fd::{AsRawFd, OwnedFd, RawFd};
 use std::cell::UnsafeCell;
+use std::sync::atomic::{AtomicU32, Ordering};
 
 
 
-use crate::condition_variables::Condition;
+use atomic_wait::{wait, wake_one};
+
+use crate::condition_variables::{Condition, FutexCondition};
 use crate::message::{Message, MonMessage, MESSAGE_SIZE};
 use crate::semaphore::BinarySemaphore;
 
@@ -258,73 +261,6 @@ impl Monitor for SemaphoreMonitor {
  * #                                                                          #
  * ############################################################################
  */
-use atomic_wait::{wait, wake_one, wake_all};
-use std::sync::atomic::{AtomicU32, Ordering};
-
-/// A condition variable implementation using futexes
-#[derive(Debug)]
-pub struct FutexCondition {
-    /// Number of threads waiting on this condition
-    waiting: AtomicU32,
-    /// Futex word for this condition variable
-    futex_word: AtomicU32,
-}
-
-impl FutexCondition {
-    pub fn new() -> Self {
-        FutexCondition {
-            waiting: AtomicU32::new(0),
-            futex_word: AtomicU32::new(0),
-        }
-    }
-
-    /// Wait on this condition variable
-    pub fn wait(&self) {
-        // Increment waiting count
-        self.waiting.fetch_add(1, Ordering::AcqRel);
-        
-        // Get current futex value
-        let current_val = self.futex_word.load(Ordering::Acquire);
-        
-        // Wait on the futex
-        wait(&self.futex_word, current_val);
-        
-        // We've been woken up, decrement waiting count
-        self.waiting.fetch_sub(1, Ordering::AcqRel);
-    }
-
-    /// Signal one waiting thread
-    pub fn signal(&self) -> bool {
-        let waiting_count = self.waiting.load(Ordering::Acquire);
-        if waiting_count > 0 {
-            // Change the futex word value to wake waiters
-            self.futex_word.fetch_add(1, Ordering::AcqRel);
-            // Wake one waiter
-            wake_one(&self.futex_word);
-            true
-        } else {
-            false
-        }
-    }
-
-    /// Broadcast to all waiting threads
-    pub fn broadcast(&self) -> u32 {
-        let waiting_count = self.waiting.load(Ordering::Acquire);
-        if waiting_count > 0 {
-            // Change the futex word value to wake waiters
-            self.futex_word.fetch_add(1, Ordering::AcqRel);
-            // Wake all waiters
-            wake_all(&self.futex_word);
-            waiting_count
-        } else {
-            0
-        }
-    }
-
-    pub fn waiting_count(&self) -> u32 {
-        self.waiting.load(Ordering::Acquire)
-    }
-}
 
 /// Monitor implementation using futexes with Hoare semantics
 #[derive(Debug)]
