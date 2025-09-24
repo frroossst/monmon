7357db6ba90ef28429e1f32519dbf6bab5b14238
Adhyan H <adhyanpatel@gmail.com>
2025-09-24 16:07:18 -0600

    re-rordered tests

--- monmon-dbg/src/main.rs
+++ monmon-dbg/src/main.rs
@@ -97,14 +97,14 @@ fn main() {
         race(RaceKind::UnsafeAccum, config.clone());
         race(RaceKind::UnsafeBuffer, config.clone());
     }
-    race(RaceKind::SemaphoreMonitorAccum, config.clone());
-    race(RaceKind::SemaphoreMonitorBuffer, config.clone());
     race(RaceKind::StdlibMutexAccum, config.clone());
     race(RaceKind::StdlibMutexBuffer, config.clone());
+    race(RaceKind::SemaphoreMonitorAccum, config.clone());
+    race(RaceKind::SemaphoreMonitorBuffer, config.clone());
+    race(RaceKind::FutexMonitorAccum, config.clone());
+    race(RaceKind::FutexMonitorBuffer, config.clone());
     race(RaceKind::BinarySemaphoreAccum, config.clone());
     race(RaceKind::BinarySemaphoreBuffer, config.clone());
     race(RaceKind::HappyLockAccum, config.clone());
     race(RaceKind::HappyLockBuffer, config.clone());
-    race(RaceKind::FutexMonitorAccum, config.clone());
-    race(RaceKind::FutexMonitorBuffer, config.clone());
 }
