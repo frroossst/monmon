8da7083e3ed8fe69543a55c5d0c2ca96facbb4fa
Adhyan H <adhyanpatel@gmail.com>
2025-09-25 09:06:53 -0600

    updated docs to include rust code

--- monmon-impl/src/ipc_monitor.rs
+++ monmon-impl/src/ipc_monitor.rs
@@ -15,71 +15,9 @@ static IPCSERVER_SINGLETON: OnceCell<AtomicBool> = OnceCell::new();
 /// Uses Send/Receive/Reply, Send(s) are blocking
 #[derive(Debug)]
 pub struct IPCMonitorServer {
-    tx: OwnedFd,
-    rx: OwnedFd,
-    clients: Vec<OwnedFd>,
-    conditions: Vec<Condition>,
-}
-
-impl IPCMonitorServer {
-    pub fn new(num_conds: usize) -> Self {
-        let (tx, rx) = nix::unistd::pipe().unwrap();
-
-        let mut conditions: Vec<Condition> = Vec::with_capacity(num_conds);
-        for _ in 0..num_conds {
-            let condition = Condition::default();
-            conditions.push(condition);
-        }
-
-        IPCMonitorServer {
-            tx,
-            rx,
-            clients: Vec::new(),
-            conditions,
-        }
-    }
-
-    pub fn connection(&self) -> (RawFd, RawFd) {
-        (self.tx.as_raw_fd(), self.rx.as_raw_fd())
-    }
-
-    /// loop {
-    ///     let msg = self.receive();
-    ///     
-    /// }
-    pub fn receive(&mut self) -> Message {
-        let mut buffer = vec![0u8; MESSAGE_SIZE];
-        let bytes_read = nix::unistd::read(self.rx.as_raw_fd(), &mut buffer).unwrap();
-        if bytes_read != MESSAGE_SIZE {
-            panic!("Failed to read the full message");
-        }
-
-        if let Ok(msg) = Message::decode(&buffer) {
-            msg
-        } else {
-            eprintln!("{:?}", buffer);
-            panic!("Failed to decode message");
-        }
-    }
-
-    pub fn serve(&mut self) {
-        let _singleton = IPCSERVER_SINGLETON.get_or_init(|| AtomicBool::new(false));
-
-        loop {
-            let msg = self.receive();
-            dbg!(msg);
-        }
-    }
 }
 
 /// Client side of IPC monitor implementation
 pub struct IPCMonitorClient {
-    tx: RawFd,
-    rx: RawFd,
 }
 
-impl IPCMonitorClient {
-    pub fn new(tx: RawFd, rx: RawFd) -> Self {
-        unimplemented!()
-    }
-}
