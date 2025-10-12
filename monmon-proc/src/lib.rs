d168e5fadd238dbb0a01587500fbe949b91810a3
Adhyan H <adhyanpatel@gmail.com>
2025-10-11 22:07:36 -0600

    proc macro now accepts a attrs

--- monmon-proc/src/lib.rs
+++ monmon-proc/src/lib.rs
@@ -1,6 +1,6 @@
 use syn::Error;
 
-use quote::{quote, ToTokens};
+use quote::quote;
 use proc_macro::TokenStream;
 use syn::{parse_macro_input, spanned::Spanned};
 
@@ -9,11 +9,11 @@ fn report_error<T: Spanned>(span: T, message: &str) -> TokenStream {
 }
 
 #[proc_macro_attribute]
-pub fn synchronised(_attr: TokenStream, item: TokenStream) -> TokenStream {
+pub fn synchronised(attr: TokenStream, item: TokenStream) -> TokenStream {
+
     let input = parse_macro_input!(item as syn::ItemFn);
 
     let mut mon_arg = None;
-
     let fn_args = &input.sig.inputs;
 
     // find the argument that has monitor type 
@@ -36,21 +36,55 @@ pub fn synchronised(_attr: TokenStream, item: TokenStream) -> TokenStream {
         }
     }
 
-    if mon_arg.is_none() {
-        return report_error(input.sig.paren_token.span.clone(), "No argument of type &impl Monitor found");
-    }
+    // Determine which monitor to use
+    let monitor_expr = if attr.is_empty() {
+        // No attribute specified, try to find monitor parameter automatically
+        if mon_arg.is_none() {
+            return report_error(input.sig.paren_token.span.clone(), "No argument of type &impl Monitor found");
+        }
+        
+        // Convert the pattern to an expression for consistency
+        let mon_pat = mon_arg.unwrap();
+        syn::parse_quote! { #mon_pat }
+    } else {
+        // Parse the attribute as an expression
+        let attr_str = attr.to_string();
+        let attr_str = attr_str.trim();
+        let attr_expr: syn::Expr = match syn::parse_str(attr_str) {
+            Ok(expr) => expr,
+            Err(_) => return report_error(input.sig.paren_token.span.clone(), "Failed to parse attribute as expression"),
+        };
 
-    let mon_arg = mon_arg.unwrap();
+        // Handle different expression types
+        match &attr_expr {
+            syn::Expr::Path(_) => {
+                // Simple path like `monitor`
+                attr_expr
+            },
+            syn::Expr::Reference(ref_expr) => {
+                // Reference like `&self.monitor` - use the inner expression
+                (*ref_expr.expr).clone()
+            },
+            syn::Expr::Field(_) => {
+                // Field access like `self.monitor` - use directly
+                attr_expr
+            },
+            _ => {
+                return report_error(input.sig.paren_token.span.clone(), "Attribute expression must be a path, reference, or field access");
+            },
+        }
+    };
+
     let block = &input.block;
 
     // add monitor.enter() at the begining of the block
     let enter_stmt: syn::Stmt = syn::parse_quote! {
-        #mon_arg.enter();
+        #monitor_expr.enter();
     };
 
     // add monitor.leave() at the end of the block
     let leave_stmt: syn::Stmt = syn::parse_quote! {
-        #mon_arg.leave();
+        #monitor_expr.leave();
     };
 
     let wrapped_block_stmt: syn::Stmt = syn::parse_quote! {
